77eeeb6820a9ecbb23a6bbd5dc535c4d30a983b4
aksuta <an.aksuta@gmail.com>
2021-01-18 23:09:46 +0200

    lesson#36 full

--- src/js/singleItem/singleItemController.js
+++ src/js/singleItem/singleItemController.js
@@ -1,9 +1,16 @@
 import SingleItem from './singleItemModel.js';
+import * as view from './singleItemView.js';
 
-export default function (state){
+export default async function (state){
     console.log('Single Item Controller STARTED!!!');
 
     state.singleItem = new SingleItem(state.routeParams);
-    state.singleItem.getItem();
+    
+    //получаем данные с сервера
+    await state.singleItem.getItem();
+
+    //отрисовываем разметку  для отдельного обьекта 
+    view.render (state.singleItem.result);
+
 
 }
